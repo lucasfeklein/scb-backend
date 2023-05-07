d9ee3e230d78606c8afe50218946d5e8e4cb3536
Lucas Klein <lucas.fklein@hotmail.com>
2023-05-07 03:04:05 -0300

    cors added

--- src/routes/publicRoutes.js
+++ src/routes/publicRoutes.js
@@ -30,7 +30,7 @@ router.post("/request-magic-link", async (req, res) => {
   });
 
   const magicLinkToken = user.emailVerificationToken;
-  const magicLink = `https://localhost:3000/auth/magic-link?token=${magicLinkToken}`;
+  const magicLink = `https://localhost:3000/magic-link?token=${magicLinkToken}`;
 
   const transporter = nodemailer.createTransport({
     host: "smtp.office365.com",
