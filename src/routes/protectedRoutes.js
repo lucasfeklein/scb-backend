2517919b58036f8e308ee822a26c99efc883e543
Lucas Klein <lucas.fklein@hotmail.com>
2023-05-15 01:14:25 -0300

    /onboarding path added

--- src/routes/protectedRoutes.js
+++ src/routes/protectedRoutes.js
@@ -9,11 +9,33 @@ import {
 
 const router = express.Router();
 
-router.post("/company", authMiddleware, async (req, res) => {
-  const { name, website } = req.body;
-  console.log("1");
+router.post("/onboarding", authMiddleware, async (req, res) => {
+  const { name, companyName } = req.body;
+
+  await prisma.user.update({
+    where: {
+      id: req.userId,
+    },
+    data: {
+      name: name,
+    },
+  });
+
+  const company = await prisma.company.create({
+    data: {
+      userId: req.userId,
+      name: companyName,
+    },
+  });
+
+  return res.json(company);
+});
+
+router.post("/widget", authMiddleware, async (req, res) => {
+  const { website } = req.body;
+
   const url = new URL(website);
-  console.log("2");
+
   const company = await prisma.company.upsert({
     where: {
       website: url.hostname,
@@ -21,15 +43,14 @@ router.post("/company", authMiddleware, async (req, res) => {
     update: {},
     create: {
       userId: req.userId,
-      name: name,
       website: url.hostname,
     },
   });
-  console.log("3");
+
   await processWebsite({
     hostname: url.hostname,
   });
-  console.log("4");
+
   return res.json(company);
 });
 
@@ -38,21 +59,17 @@ router.get("/auth-verify", authMiddleware, async (req, res) => {
     // Find the user in the database by the userId in the JWT token
     const user = await prisma.user.findUnique({
       where: { id: req.userId },
+      include: { company: true },
     });
 
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
 
-    // Check if the emailVerificationTokenExpiry has expired
-    if (user.emailVerificationTokenExpiry < Date.now()) {
-      return res
-        .status(401)
-        .json({ message: "Email verification token has expired" });
-    }
-
     // Email verification token is still valid, send back the email
-    return res.status(200).json({ email: user.email });
+    return res
+      .status(200)
+      .json({ email: user.email, name: user.name, company: user.company });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: "Internal server error" });
