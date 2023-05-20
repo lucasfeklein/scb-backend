46cdf3f84615e053a0e02568ed1a5388632e4808
Lucas Klein <lucas.fklein@hotmail.com>
2023-05-20 14:17:31 -0300

    a

--- src/index.js
+++ src/index.js
@@ -84,10 +84,7 @@ ${bigText}
         { role: "system", content: prompt },
         { role: "user", content: question },
       ],
-      temperature: 0.1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-      top_p: 1,
+      temperature: 0,
     });
 
     // Send a response back to the client
