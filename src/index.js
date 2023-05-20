03a7f9ef50382f02013d0dc5d898bd4b21dee5b5
Lucas Klein <lucas.fklein@hotmail.com>
2023-05-20 19:10:46 -0300

    best prompt so far

--- src/index.js
+++ src/index.js
@@ -71,7 +71,7 @@ wss.on("connection", async (ws, request) => {
       bigText += pageContent.replace(/\n/g, " ");
     }
 
-    const prompt = `You are a helpful chatbot who loves to help people. You're designed to answer only based on the Content provided, outputted in Markdown format. If it's not about the content say that you're not able to answer it.
+    const prompt = `You are a helpful chatbot who loves to help people. Your name is Chatbot and you're designed to answer only based on the Content provided, outputted in Markdown format. If it's not about the content say that you're not able to answer it.
 
 Content: ###
 ${bigText}
