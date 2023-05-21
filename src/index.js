c9610c02977b251ed4a0777008cc543040e9f26f
Lucas Klein <lucas.fklein@hotmail.com>
2023-05-21 00:49:05 -0300

    removed websocket, now works with http request

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import { createServer } from "http";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { PineconeStore } from "langchain/vectorstores/pinecone";
 import { Configuration, OpenAIApi } from "openai";
-import { WebSocketServer } from "ws";
 import app from "./app.js";
 import { env } from "./config/env.js";
 import { pinecone } from "./config/pinecone.js";
@@ -18,7 +17,6 @@ const openai = new OpenAIApi(configuration);
 const embeddings = new OpenAIEmbeddings();
 
 const server = createServer(app);
-const wss = new WebSocketServer({ server });
 
 async function verifyHostname(hostname) {
   if (!(hostname && hostname.length > 0)) return false;
@@ -85,66 +83,6 @@ ${bigText}
     res.status(500).send("Internal server error");
   }
 });
-
-// wss.on("connection", async (ws, request) => {
-//   const url = new URL(request.url, `http://${request.headers.host}`);
-//   const hostname = url.searchParams.get("hostname");
-
-//   const isHostnameValid = await verifyHostname(hostname);
-
-//   if (!isHostnameValid) {
-//     ws.send("Invalid hostname");
-//     ws.close();
-//     return;
-//   }
-
-//   await pinecone.init({
-//     apiKey: env.PINECONE_API_KEY,
-//     environment: env.PINECONE_API_ENV,
-//   });
-//   const pineconeIndex = pinecone.Index(env.PINECONE_INDEX);
-//   const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
-//     pineconeIndex,
-//     namespace: hostname,
-//   });
-
-//   console.log("Client connected");
-
-//   ws.on("message", async (message) => {
-//     console.log(`Received message from client: ${message}`);
-
-//     const question = message.toString();
-
-//     const documents = await vectorStore.similaritySearch(question, 8);
-//     let bigText = "";
-
-//     console.log(bigText);
-
-//     for (let i = 0; i < documents.length; i++) {
-//       let pageContent = documents[i].pageContent;
-//       bigText += pageContent.replace(/\n/g, " ");
-//     }
-
-//     const prompt = `You are a helpful chatbot who loves to help people. Your name is Chatbot and you're designed to answer only based on the Content provided, outputted in Markdown format. If it's not about the content say that you're not able to answer it.
-
-// Content: ###
-// ${bigText}
-// ###
-// `;
-
-//     const completion = await openai.createChatCompletion({
-//       model: "gpt-3.5-turbo",
-//       messages: [
-//         { role: "system", content: prompt },
-//         { role: "user", content: question },
-//       ],
-//       temperature: 0,
-//     });
-
-//     // Send a response back to the client
-//     ws.send(`${completion.data.choices[0].message?.content}`);
-//   });
-// });
 
 app.get("/teste", (req, res) => {
   processWebsite({ hostname: "dynamicpoa.com" });
